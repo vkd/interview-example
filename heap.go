dc078d488d1cf3f2a08a457309d607a7c391854c
Dmitry Kutakov <vkd.castle@gmail.com>
2020-02-11 14:27:23 +0100

    Create heap.go

---
+++ heap.go
@@ -0,0 +1,14 @@
+package main
+
+// write simple Heap implementation
+type Heap struct{}
+
+type Pointer struct{}
+
+func (h *Heap) Malloc(size int) Pointer {
+	panic("Not implemented")
+}
+
+func (h *Heap) Free(p Pointer) {
+	panic("Not implemented")
+}
